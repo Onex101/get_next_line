14539d78b53cff0066468343673f33d3d3230335
Xeno RHODA <xrhoda@c1r2s1.wethinkcode.co.za>
2018-06-15 07:59:32 +0200

    Finished testing, needs to be validated

--- get_next_line.c
+++ get_next_line.c
@@ -13,7 +13,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "get_next_line.h"
-//
 #include <stdio.h>
 
 static	t_list	*new_file(t_list **file_list, int fd)
@@ -41,12 +40,11 @@ static	void	gnl_cat(char **line, t_list *file)
 
 	size_line = ft_strclen(file->content, '\n');
 	*line = ft_strsub(file->content, 0, size_line);
-	if (((char *)(file->content))[size_line + 1] != '\0') //  size_line < (int)ft_strlen(file->content))
+	if (((char *)(file->content))[size_line] != '\0')
 	{
 		tmp = ft_strdup(file->content + (size_line + 1));
 		free(file->content);
 		file->content = tmp;
-		//printf("file->content: %s\n", file->content);
 	}
 	else
 		ft_strclr(file->content);
@@ -60,24 +58,19 @@ static	int		gnl_read(char **line, t_list *file, char *buf)
 	while ((num_bytes = read(file->content_size, buf, BUFF_SIZE)) > 0)
 	{
 		buf[num_bytes] = '\0';
-		//printf("buf: %s\n", buf);
 		if (!(tmp = ft_strjoin(file->content, buf)))
 			return (-1);
-		//printf("tmp: %s\n", tmp);
 		free(file->content);
-		//printf("tmp after free: %s\n", tmp);
 		file->content = ft_strdup(tmp);
 		free(tmp);
-		//printf("file->content: %s\n", file->content);
 		if (ft_strchr(buf, '\n'))
 			break ;
 		ft_strclr(buf);
 	}
-	//printf("tmp: %s\n", tmp);
-	//printf("num_bytes: %d file->content: %s\n", num_bytes, file->content);
 	if (num_bytes < BUFF_SIZE && !ft_strlen(file->content))
 		return (0);
 	gnl_cat(line, file);
+	ft_strclr(buf);
 	return (1);
 }
 
@@ -91,12 +84,11 @@ int				get_next_line(const int fd, char **line)
 	if ((fd < 0 || !line || read(fd, 0, 0) < 0))
 		return (-1);
 	file = new_file(&file_list, fd);
+	buf[BUFF_SIZE] = '\0';
 	if ((i = gnl_read(line, file, buf)) == 0)
-	{	
+	{
 		ft_strclr(file->content);
 		return (0);
 	}
-	//printf("%d\n", i);
 	return (i);
-
 }
